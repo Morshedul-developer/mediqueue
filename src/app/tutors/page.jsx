1662b773d2fc154000eca94a44afe2556c5a5e96
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-06-28 11:19:53 +0600

    made the tutors page

--- src/app/tutors/page.jsx
+++ src/app/tutors/page.jsx
@@ -1,10 +1,22 @@
+import TutorCard from "@/components/ui/TutorCard";
 
-const TutorPage = () => {
-    return (
-        <div>
-            Tutor Page
-        </div>
-    );
+const TutorsPage = async () => {
+  const res = await fetch(`http://localhost:5000/tutors`);
+  const tutors = await res.json();
+  return (
+    <section className="max-w-7xl mx-auto space-y-10 px-4 md:px-0 my-20">
+      <div className="animate__animated animate__fadeInDown">
+        <h3 className="text-2xl md:text-4xl font-bold text-center">
+          All Tutors
+        </h3>
+      </div>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {tutors.map((tutor) => (
+          <TutorCard key={tutor._id} tutor={tutor} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
-export default TutorPage;
+export default TutorsPage;
