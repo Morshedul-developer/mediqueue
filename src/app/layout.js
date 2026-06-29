7de1a60e9cc19e0513bf0c1e92e3bf2b8293fc06
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-06-29 16:18:22 +0600

    added inter font to the project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import { ToastContainer } from "react-toastify";
 import Providers from "@/providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const InterFont = Inter({
   subsets: ["latin"],
 });
 
@@ -27,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${InterFont.className} h-full antialiased`}
     >
       <body>
         <Providers>
